69db5c71dc01ff54e1307cc2aac3c9bfa4d8af48
Ari219 <ariiimorales21@gmail.com>
2023-06-08 15:16:05 -0600

    Dataframe
    Se ingresa un dataframe de pastelería

--- tarea1.py
+++ tarea1.py
@@ -1,2 +1,10 @@
 import pandas as pd 
 import numpy as np 
+
+pastelería={"Nombre Producto":["vainilla", "chocolate", "red velvet", "marmoleado"], 
+"Cantidad de Ventas":[25,15,20,10], 
+"Costo de Produccion":[187500, 112500, 160000, 75000],
+"Margen de Beneficio":[187500, 112500, 160000, 75000],  
+"Precio de Venta": [375000, 225000, 360000, 150000]}
+df = pd.DataFrame(pastelería)
+print(df)
