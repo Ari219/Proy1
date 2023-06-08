cc8411d7ea1179f53d59c7a2d26d0df567ed8cc9
DonMarco <marco500ag@gmail.com>
2023-06-08 16:37:33 -0600

    Df Marco
    Se añadio df sobre peli

--- tarea1.py
+++ tarea1.py
@@ -14,3 +14,9 @@ nuevos_datos={"Pago":["Transferencia","Transferencia", "Transferencia", "Transfe
 pt["Pago"] = nuevos_datos["Pago"]
 pt["Envío"] = nuevos_datos["Envío"]
 print(pt)
+pelicula={"Nombre": ["Super Mario Bros", "The Whale", "Spiderman: Across", "Notting Hill", "Kiki:entregas a domicilio", "Green Book"], 
+"Género":["animada", "drama", "animada", "romance", "anime", "Drama" ],
+"Duración": ["1h32m", "1h57m", "2h20m", "2h04m", "1h42m", "2h10m"],
+"Puntaje Rotten Tomatoes": [60, 65, 96, 85, 99, 77]}
+pl= pd.DataFrame(pelicula)
+print(pl)
